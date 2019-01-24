fa4a8f966b258b2720e06efd87baa4dec1c39b20
EliTheCoder <ex.games.org@gmail.com>
2019-01-24 13:32:44 -0600

    added map function for p5.js

--- eliapi.js
+++ eliapi.js
@@ -37,3 +37,7 @@ exports.logMessage = (type, message) => {
 exports.random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+exports.map = (n, start1, stop1, start2, stop2) => {
+  return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+}
